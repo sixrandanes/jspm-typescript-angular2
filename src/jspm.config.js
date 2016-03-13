62a07d4f64347fcf05755a508da2de84ada9db78
Sylvain <lbsylvain1@aol.com>
2016-03-13 14:02:15 +1100

    feat(css): remove foundation => ajout mdl

--- src/jspm.config.js
+++ src/jspm.config.js
@@ -30,10 +30,10 @@ SystemJS.config({
     "es6-promise": "npm:es6-promise@3.1.2",
     "es6-shim": "npm:es6-shim@0.33.13",
     "events": "github:jspm/nodelibs-events@0.2.0-alpha",
-    "foundation": "npm:foundation-sites@6.2.0",
     "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
     "image": "github:systemjs/plugin-image@0.1.0",
     "json": "github:systemjs/plugin-json@0.1.0",
+    "material-design-lite": "github:google/material-design-lite@1.1.2",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "reflect-metadata": "npm:reflect-metadata@0.1.3",
@@ -76,11 +76,6 @@ SystemJS.config({
     "github:jspm/nodelibs-url@0.2.0-alpha": {
       "map": {
         "url-browserify": "npm:url@0.11.0"
-      }
-    },
-    "npm:ansi-styles@2.2.0": {
-      "map": {
-        "color-convert": "npm:color-convert@1.0.0"
       }
     },
     "npm:asn1.js@4.5.1": {
@@ -137,29 +132,9 @@ SystemJS.config({
         "isarray": "npm:isarray@1.0.0"
       }
     },
-    "npm:camelcase-keys@2.0.0": {
-      "map": {
-        "camelcase": "npm:camelcase@2.1.0",
-        "map-obj": "npm:map-obj@1.0.1"
-      }
-    },
-    "npm:chalk@1.1.1": {
-      "map": {
-        "ansi-styles": "npm:ansi-styles@2.2.0",
-        "escape-string-regexp": "npm:escape-string-regexp@1.0.5",
-        "has-ansi": "npm:has-ansi@2.0.0",
-        "strip-ansi": "npm:strip-ansi@3.0.1",
-        "supports-color": "npm:supports-color@2.0.0"
-      }
-    },
     "npm:cipher-base@1.0.2": {
       "map": {
         "inherits": "npm:inherits@2.0.1"
-      }
-    },
-    "npm:concat-with-sourcemaps@1.0.4": {
-      "map": {
-        "source-map": "npm:source-map@0.5.3"
       }
     },
     "npm:create-ecdh@4.0.0": {
@@ -196,12 +171,6 @@ SystemJS.config({
         "randombytes": "npm:randombytes@2.0.3"
       }
     },
-    "npm:dateformat@1.0.12": {
-      "map": {
-        "get-stdin": "npm:get-stdin@4.0.1",
-        "meow": "npm:meow@3.7.0"
-      }
-    },
     "npm:des.js@1.0.0": {
       "map": {
         "inherits": "npm:inherits@2.0.1",
@@ -215,11 +184,6 @@ SystemJS.config({
         "randombytes": "npm:randombytes@2.0.3"
       }
     },
-    "npm:duplexer2@0.0.2": {
-      "map": {
-        "readable-stream": "npm:readable-stream@1.1.13"
-      }
-    },
     "npm:elliptic@6.2.3": {
       "map": {
         "bn.js": "npm:bn.js@4.11.0",
@@ -228,82 +192,9 @@ SystemJS.config({
         "inherits": "npm:inherits@2.0.1"
       }
     },
-    "npm:error-ex@1.3.0": {
-      "map": {
-        "is-arrayish": "npm:is-arrayish@0.2.1"
-      }
-    },
     "npm:evp_bytestokey@1.0.0": {
       "map": {
         "create-hash": "npm:create-hash@1.1.2"
-      }
-    },
-    "npm:fancy-log@1.2.0": {
-      "map": {
-        "chalk": "npm:chalk@1.1.1",
-        "time-stamp": "npm:time-stamp@1.0.0"
-      }
-    },
-    "npm:find-up@1.1.2": {
-      "map": {
-        "path-exists": "npm:path-exists@2.1.0",
-        "pinkie-promise": "npm:pinkie-promise@2.0.0"
-      }
-    },
-    "npm:foundation-sites@6.2.0": {
-      "map": {
-        "jquery": "npm:jquery@2.2.1",
-        "what-input": "npm:what-input@1.2.5"
-      }
-    },
-    "npm:glogg@1.0.0": {
-      "map": {
-        "sparkles": "npm:sparkles@1.0.0"
-      }
-    },
-    "npm:gulp-header@1.7.1": {
-      "map": {
-        "concat-with-sourcemaps": "npm:concat-with-sourcemaps@1.0.4",
-        "gulp-util": "npm:gulp-util@3.0.7",
-        "object-assign": "npm:object-assign@3.0.0",
-        "through2": "npm:through2@2.0.1"
-      }
-    },
-    "npm:gulp-util@3.0.7": {
-      "map": {
-        "array-differ": "npm:array-differ@1.0.0",
-        "array-uniq": "npm:array-uniq@1.0.2",
-        "beeper": "npm:beeper@1.1.0",
-        "chalk": "npm:chalk@1.1.1",
-        "dateformat": "npm:dateformat@1.0.12",
-        "fancy-log": "npm:fancy-log@1.2.0",
-        "gulplog": "npm:gulplog@1.0.0",
-        "has-gulplog": "npm:has-gulplog@0.1.0",
-        "lodash._reescape": "npm:lodash._reescape@3.0.0",
-        "lodash._reevaluate": "npm:lodash._reevaluate@3.0.0",
-        "lodash._reinterpolate": "npm:lodash._reinterpolate@3.0.0",
-        "lodash.template": "npm:lodash.template@3.6.2",
-        "minimist": "npm:minimist@1.2.0",
-        "multipipe": "npm:multipipe@0.1.2",
-        "object-assign": "npm:object-assign@3.0.0",
-        "replace-ext": "npm:replace-ext@0.0.1",
-        "through2": "npm:through2@2.0.1",
-        "vinyl": "npm:vinyl@0.5.3"
-      }
-    },
-    "npm:gulplog@1.0.0": {
-      "map": {
-        "glogg": "npm:glogg@1.0.0"
-      }
-    },
-    "npm:has-ansi@2.0.0": {
-      "map": {
-        "ansi-regex": "npm:ansi-regex@2.0.0"
-      }
-    },
-    "npm:has-gulplog@0.1.0": {
-      "map": {
-        "sparkles": "npm:sparkles@1.0.0"
       }
     },
     "npm:hash.js@1.0.3": {
@@ -311,98 +202,10 @@ SystemJS.config({
         "inherits": "npm:inherits@2.0.1"
       }
     },
-    "npm:indent-string@2.1.0": {
-      "map": {
-        "repeating": "npm:repeating@2.0.0"
-      }
-    },
-    "npm:is-builtin-module@1.0.0": {
-      "map": {
-        "builtin-modules": "npm:builtin-modules@1.1.1"
-      }
-    },
-    "npm:is-finite@1.0.1": {
-      "map": {
-        "number-is-nan": "npm:number-is-nan@1.0.0"
-      }
-    },
-    "npm:load-json-file@1.1.0": {
-      "map": {
-        "graceful-fs": "npm:graceful-fs@4.1.3",
-        "parse-json": "npm:parse-json@2.2.0",
-        "pify": "npm:pify@2.3.0",
-        "pinkie-promise": "npm:pinkie-promise@2.0.0",
-        "strip-bom": "npm:strip-bom@2.0.0"
-      }
-    },
-    "npm:lodash.escape@3.2.0": {
-      "map": {
-        "lodash._root": "npm:lodash._root@3.0.1"
-      }
-    },
-    "npm:lodash.keys@3.1.2": {
-      "map": {
-        "lodash._getnative": "npm:lodash._getnative@3.9.1",
-        "lodash.isarguments": "npm:lodash.isarguments@3.0.8",
-        "lodash.isarray": "npm:lodash.isarray@3.0.4"
-      }
-    },
-    "npm:lodash.template@3.6.2": {
-      "map": {
-        "lodash._basecopy": "npm:lodash._basecopy@3.0.1",
-        "lodash._basetostring": "npm:lodash._basetostring@3.0.1",
-        "lodash._basevalues": "npm:lodash._basevalues@3.0.0",
-        "lodash._isiterateecall": "npm:lodash._isiterateecall@3.0.9",
-        "lodash._reinterpolate": "npm:lodash._reinterpolate@3.0.0",
-        "lodash.escape": "npm:lodash.escape@3.2.0",
-        "lodash.keys": "npm:lodash.keys@3.1.2",
-        "lodash.restparam": "npm:lodash.restparam@3.6.1",
-        "lodash.templatesettings": "npm:lodash.templatesettings@3.1.1"
-      }
-    },
-    "npm:lodash.templatesettings@3.1.1": {
-      "map": {
-        "lodash._reinterpolate": "npm:lodash._reinterpolate@3.0.0",
-        "lodash.escape": "npm:lodash.escape@3.2.0"
-      }
-    },
-    "npm:loud-rejection@1.3.0": {
-      "map": {
-        "array-find-index": "npm:array-find-index@1.0.1",
-        "signal-exit": "npm:signal-exit@2.1.2"
-      }
-    },
-    "npm:meow@3.7.0": {
-      "map": {
-        "camelcase-keys": "npm:camelcase-keys@2.0.0",
-        "decamelize": "npm:decamelize@1.2.0",
-        "loud-rejection": "npm:loud-rejection@1.3.0",
-        "map-obj": "npm:map-obj@1.0.1",
-        "minimist": "npm:minimist@1.2.0",
-        "normalize-package-data": "npm:normalize-package-data@2.3.5",
-        "object-assign": "npm:object-assign@4.0.1",
-        "read-pkg-up": "npm:read-pkg-up@1.0.1",
-        "redent": "npm:redent@1.0.0",
-        "trim-newlines": "npm:trim-newlines@1.0.0"
-      }
-    },
     "npm:miller-rabin@4.0.0": {
       "map": {
         "bn.js": "npm:bn.js@4.11.0",
         "brorand": "npm:brorand@1.0.5"
-      }
-    },
-    "npm:multipipe@0.1.2": {
-      "map": {
-        "duplexer2": "npm:duplexer2@0.0.2"
-      }
-    },
-    "npm:normalize-package-data@2.3.5": {
-      "map": {
-        "hosted-git-info": "npm:hosted-git-info@2.1.4",
-        "is-builtin-module": "npm:is-builtin-module@1.0.0",
-        "semver": "npm:semver@5.1.0",
-        "validate-npm-package-license": "npm:validate-npm-package-license@3.0.1"
       }
     },
     "npm:parse-asn1@5.0.0": {
@@ -414,31 +217,9 @@ SystemJS.config({
         "pbkdf2": "npm:pbkdf2@3.0.4"
       }
     },
-    "npm:parse-json@2.2.0": {
-      "map": {
-        "error-ex": "npm:error-ex@1.3.0"
-      }
-    },
-    "npm:path-exists@2.1.0": {
-      "map": {
-        "pinkie-promise": "npm:pinkie-promise@2.0.0"
-      }
-    },
-    "npm:path-type@1.1.0": {
-      "map": {
-        "graceful-fs": "npm:graceful-fs@4.1.3",
-        "pify": "npm:pify@2.3.0",
-        "pinkie-promise": "npm:pinkie-promise@2.0.0"
-      }
-    },
     "npm:pbkdf2@3.0.4": {
       "map": {
         "create-hmac": "npm:create-hmac@1.1.4"
-      }
-    },
-    "npm:pinkie-promise@2.0.0": {
-      "map": {
-        "pinkie": "npm:pinkie@2.0.4"
       }
     },
     "npm:public-encrypt@4.0.0": {
@@ -448,28 +229,6 @@ SystemJS.config({
         "create-hash": "npm:create-hash@1.1.2",
         "parse-asn1": "npm:parse-asn1@5.0.0",
         "randombytes": "npm:randombytes@2.0.3"
-      }
-    },
-    "npm:read-pkg-up@1.0.1": {
-      "map": {
-        "find-up": "npm:find-up@1.1.2",
-        "read-pkg": "npm:read-pkg@1.1.0"
-      }
-    },
-    "npm:read-pkg@1.1.0": {
-      "map": {
-        "load-json-file": "npm:load-json-file@1.1.0",
-        "normalize-package-data": "npm:normalize-package-data@2.3.5",
-        "path-type": "npm:path-type@1.1.0"
-      }
-    },
-    "npm:readable-stream@1.1.13": {
-      "map": {
-        "core-util-is": "npm:core-util-is@1.0.2",
-        "inherits": "npm:inherits@2.0.1",
-        "isarray": "npm:isarray@0.0.1",
-        "stream-browserify": "npm:stream-browserify@1.0.0",
-        "string_decoder": "npm:string_decoder@0.10.31"
       }
     },
     "npm:readable-stream@2.0.5": {
@@ -482,37 +241,9 @@ SystemJS.config({
         "util-deprecate": "npm:util-deprecate@1.0.2"
       }
     },
-    "npm:redent@1.0.0": {
-      "map": {
-        "indent-string": "npm:indent-string@2.1.0",
-        "strip-indent": "npm:strip-indent@1.0.1"
-      }
-    },
-    "npm:repeating@2.0.0": {
-      "map": {
-        "is-finite": "npm:is-finite@1.0.1"
-      }
-    },
     "npm:sha.js@2.4.5": {
       "map": {
         "inherits": "npm:inherits@2.0.1"
-      }
-    },
-    "npm:spdx-correct@1.0.2": {
-      "map": {
-        "spdx-license-ids": "npm:spdx-license-ids@1.2.0"
-      }
-    },
-    "npm:spdx-expression-parse@1.0.2": {
-      "map": {
-        "spdx-exceptions": "npm:spdx-exceptions@1.0.4",
-        "spdx-license-ids": "npm:spdx-license-ids@1.2.0"
-      }
-    },
-    "npm:stream-browserify@1.0.0": {
-      "map": {
-        "inherits": "npm:inherits@2.0.1",
-        "readable-stream": "npm:readable-stream@1.1.13"
       }
     },
     "npm:stream-browserify@2.0.1": {
@@ -521,49 +252,10 @@ SystemJS.config({
         "readable-stream": "npm:readable-stream@2.0.5"
       }
     },
-    "npm:strip-ansi@3.0.1": {
-      "map": {
-        "ansi-regex": "npm:ansi-regex@2.0.0"
-      }
-    },
-    "npm:strip-bom@2.0.0": {
-      "map": {
-        "is-utf8": "npm:is-utf8@0.2.1"
-      }
-    },
-    "npm:strip-indent@1.0.1": {
-      "map": {
-        "get-stdin": "npm:get-stdin@4.0.1"
-      }
-    },
-    "npm:through2@2.0.1": {
-      "map": {
-        "readable-stream": "npm:readable-stream@2.0.5",
-        "xtend": "npm:xtend@4.0.1"
-      }
-    },
     "npm:url@0.11.0": {
       "map": {
         "punycode": "npm:punycode@1.3.2",
         "querystring": "npm:querystring@0.2.0"
-      }
-    },
-    "npm:validate-npm-package-license@3.0.1": {
-      "map": {
-        "spdx-correct": "npm:spdx-correct@1.0.2",
-        "spdx-expression-parse": "npm:spdx-expression-parse@1.0.2"
-      }
-    },
-    "npm:vinyl@0.5.3": {
-      "map": {
-        "clone": "npm:clone@1.0.2",
-        "clone-stats": "npm:clone-stats@0.0.1",
-        "replace-ext": "npm:replace-ext@0.0.1"
-      }
-    },
-    "npm:what-input@1.2.5": {
-      "map": {
-        "gulp-header": "npm:gulp-header@1.7.1"
       }
     },
     "npm:zone.js@0.5.15": {
