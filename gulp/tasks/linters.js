68f7922156caf5fd61b98bfa3e500aadff0b1159
sixrandanes <sylvain.leborgne@gmail.com>
2016-04-02 22:20:32 +1100

    chore(project): fix

--- gulp/tasks/linters.js
+++ gulp/tasks/linters.js
@@ -6,49 +6,6 @@
             path = require('path'),
             notifier = require('node-notifier');
 
-        // Custom linting reporter used for error notify
-        var jsHintErrorNotifier = function (file, cb) {
-            return map(function (file, cb) {
-                if ('jshint' in file && !file.jshint.success) {
-                    file.jshint.results.forEach(function (err) {
-                        if (err) {
-                            // Error message
-                            var msg = [
-                                path.basename(file.path),
-                                'Line: ' + err.error.line,
-                                'Reason: ' + err.error.reason
-                            ];
-                            notifier.notify({
-                                'title': 'Erreur',
-                                'message': msg.join('\n'),
-                                'icon': path.join(__dirname, '..', 'node_modules/gulp-notify/assets', 'gulp-error.png'),
-                            });
-                        }
-                    });
-                }
-                return cb(null, file);
-            });
-        }
-
-        pipes.jsLinters = function () {
-            return gulp.src(['./' + options.src + '/**/**.js', '!./app/jspm_packages/**',  '!./app/assets/**'])
-                .pipe(pipes.plumberTask())
-                .pipe($.jshint('.jshintrc'))
-                .pipe($.jscs())
-                .pipe($.jscsStylish.combineWithHintResults())
-                .pipe($.jshint.reporter('jshint-stylish')) // log style errors
-                .pipe($.jshint.reporter($.jshintXmlFileReporter))
-                .pipe(jsHintErrorNotifier())
-                .on('end', $.jshintXmlFileReporter.writeFile({
-                    format: 'checkstyle',
-                    filePath: 'jshint-checkstyle.xml',
-                    alwaysReport: true
-                }))
-                .on('data', function (chunk) {
-                    //console.log('got %s bytes of data', chunk);
-                });
-        };
-
         pipes.tsLinters = function () {
             return gulp.src(['./' + options.src + '/**/**.ts', '!./app/jspm_packages/**',  '!./app/assets/**'])
                 .pipe($.tslint())
