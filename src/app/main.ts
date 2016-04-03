7d8ab7e8ae574bea093523db864db048fca41267
sixrandanes <sylvain.leborgne@gmail.com>
2016-04-03 11:41:32 +1100

    feat(index.html): optimisation imports

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,7 @@
 /**
  * Created by Sylvain on 07/03/2016.
  */
+import 'angular2/bundles/angular2-polyfills.min';
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
 import {HTTP_PROVIDERS} from 'angular2/http';
